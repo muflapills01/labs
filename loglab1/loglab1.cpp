bf5d9308b306095edb117cdfaf449239a9e1f86e
mufla <f96t@ya.ru>
2024-09-06 10:26:24 +0300

    Сделал первое задание

---
+++ loglab1/loglab1.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <limits.h>
+
+int main()
+{
+	int x[10] = { 0 };
+	int min = INT_MAX, max = INT_MIN;
+
+	for (int i = 0; i < 10; i++)
+	{
+		x[i] = (i * 3) % 5;
+		printf("%d ", x[i]);
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		if (x[i] < min)
+		{
+			min = x[i];
+		}
+		if (x[i] > max)
+		{
+			max = x[i];
+		}
+	}
+	printf("\nmax = %d, min = %d", max, min);
+	printf("\nResult = %d\n", max - min);
+	return 0;
+}
